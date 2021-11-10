f2bedae38c22c8e2ecdaa7b7e3293e90fd8bb9a5
gabrielleoyo <18377075101@163.com>
2021-11-10 20:17:30 +0800

    -

--- Day03/3.6-1二叉搜索树.c
+++ Day03/3.6-1二叉搜索树.c
@@ -33,3 +33,69 @@ Position IterFind(ElementType X,BinTree BST)
     }
     return NULL;//查找失败
 }
+
+//查找最小元素的递归函数
+Position FindMin(BinTree BST)
+{
+    if(!BST)    return NULL;//空二叉树
+    else if(!BST->Left)
+        return BST;//左叶结点
+    else 
+        return FindMin(BST->Left);//沿左分支继续查找
+}
+
+//查找最大元素的迭代函数
+Position FindMax(BinTree BST)
+{
+    if(!BST)
+        while(BST->Right)
+            BST=BST->Right;
+    return BST;
+}
+
+//插入
+BinTree Insert(ElementType X,BinTree BST)
+{
+    if(!BST)//树为空 生成并返回一个结点的二叉树
+    {
+        BST=malloc(sizeof(struct TreeNode));
+        BST->Data=X;
+        BST->Left=BST->Right=NULL;
+    }else{//查找要插入元素的位置
+        if(X<BST->Data)
+        {
+            BST->Left=Insert(X,BST->Left);
+        }else if(X>BST->Data)
+            BST->Right=Insert(X,BST->Right);
+    }
+    return BST;
+}
+
+//删除
+//删除左右结点  右子树最小值、左子树最大值
+BinTree Delete(ElementType X,BinTree BST)
+{
+    Position Tmp;
+    if(!BST)    printf("要删除的元素未找到");
+    else if(X<BST->Data)
+        BST->Left=Delete(X,BST->Left);
+    else if(X>BST->Data)
+        BST->Right=Delete(X,BST->Right);
+    else
+        if(BST->Left&&BST->Right)
+        {
+            Tmp=FindMin(BST->Right);
+            BST->Data=Tmp->Data;
+            BST->Right=Delete(BST->Data,BST->Right);
+        }else{
+            Tmp=BST;
+            if(!BST->Left)
+                BST=BST->Right;
+            else if(!BST=BST->Left)
+                BST=BST->Left;
+            free(Tmp);
+        }
+    return BST;
+}
+
+
