2129802d76bb4207bdd9c43f48cab6a372575a5d
gabrielleoyo <18377075101@163.com>
2021-11-10 20:52:28 +0800

    -

--- Day03/3.7Reversing Linked Listc.c
+++ Day03/3.7Reversing Linked Listc.c
@@ -2,3 +2,22 @@
 
 //抽象链表
 //有地方存数据、有地方存指针、指向下一节点的地址
+
+//养成一个好习惯  在链表前加一个空的头结点  方便管理和操作链表
+
+Ptr Reverse(Ptr head,int K)
+{
+    cnt=1;
+    new=head->next;
+    old=new->next;
+    while(cnt<K)
+    {
+        tmp=old->next;
+        old->next=new;
+        new=old;
+        old=tmp;
+        cnt++;
+    }
+    head->next->next=old;
+    return new;
+}
