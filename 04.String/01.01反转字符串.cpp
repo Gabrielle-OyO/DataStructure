b5018b22f433daff248d1c4d5a2ad78ed38a74ed
gabrielleoyo <18377075101@163.com>
2022-01-04 19:55:17 +0800

    -

---
+++ 04.String/01.01反转字符串.cpp
@@ -0,0 +1,22 @@
+
+
+//使用库函数
+class Solution {
+ public:
+  void reverseString(vector<char>& s) {
+    for (int i = 0, j = s.size() - 1; i < s.size() / 2; i++, j--) {
+      swap(s[i], s[j]);
+    }
+  }
+}
+// swap常用的两种实现
+
+//数值交换
+int tmp = s[i];
+s[i] = s[j];
+s[j] = tmp;
+
+//位运算
+s[i] ^= s[j];
+s[j] ^= s[i];
+s[i] ^= s[j];
