a525e9d3c0ab922cd0e2cb7215affbcbbae7f6a8
gabrielleoyo <18377075101@163.com>
2022-01-05 20:20:16 +0800

    -

--- 04.String/04左旋转字符串.cpp
+++ 04.String/04左旋转字符串.cpp
@@ -1,3 +1,12 @@
 //其实使用substr 和 反转 时间复杂度是一样的，都是$O(n) $，
-//但是使用substr申请了额外空间，所以空间复杂度是$O(n)$，而反转方法的空间复杂度是$O(1)
-//$。
+//但是使用substr申请了额外空间，所以空间复杂度是$O(n)$，而反转方法的空间复杂度是$O(1)$。
+
+class Solution {
+ public:
+  string reverseLeftWords(string s, int n) {
+    reverse(s.begin(), s.begin() + n);
+    reverse(s.begin() + n, s.end());
+    reverse(s.begin(), s.end());
+    return s;
+  }
+};
