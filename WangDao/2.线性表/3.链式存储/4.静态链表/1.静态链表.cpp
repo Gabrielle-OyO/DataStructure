5c519e637be6bb86f53feb13b4dcbb07e62a6fc6
gabrielleoyo <18377075101@163.com>
2022-06-14 22:21:01 +0800

    -

--- WangDao/2.线性表/3.链式存储/4.静态链表/1.静态链表.cpp
+++ WangDao/2.线性表/3.链式存储/4.静态链表/1.静态链表.cpp
@@ -2,8 +2,10 @@
 
 #define MaxSize 50  //最大长度
 typedef struct {    //结构类型定义
-  ElemType data;    //存储元素
+  ElemType data;    //存储数据元素
   int next;         //下一个元素的数组下标
 } SLinkList[MaxSize];
 
 //静态链表以next==-1作为结束标志
+
+// SLinkList b  --  相当于定义了一个长度为MaxSize的Node型数组
